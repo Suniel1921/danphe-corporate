1b9fbf58f60e50893d84d29195e54af90f36dd7c
suni <sunielsharma1921@gmail.com>
2024-07-26 14:59:29 +0545

    banner logo tst

--- frontend/src/components/topBanner/Banner.jsx
+++ frontend/src/components/topBanner/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
             <div className="banner">
                 <img className='NepalTechlogo' src="/img/nti.png" alt="Nepal Tech Logo" />
                 <div className="bannerLink list">
-                    <NavLink to={'/cart'}><IoCallSharp className='icons'/>+977 98000000</NavLink>
+                    {/* <NavLink to={'/cart'}><IoCallSharp className='icons'/>+977 98000000</NavLink> */}
                     <NavLink to={'/cart'}><FaShoppingCart className='icons'/></NavLink>
                     <NavLink to={'/cart'}><FaUserLarge className='icons'/></NavLink>
                 </div>
