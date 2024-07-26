2c6f6d746578864a91a3884dd7d4810b2ef9745d
suni <sunielsharma1921@gmail.com>
2024-07-26 12:09:47 +0545

    add home slider component

---
+++ frontend/src/components/slider/HomeSlider.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../slider/homeSlider.css'
+
+const HomeSlider = () => {
+  return (
+    <>
+    <div className="homeSlider">
+        <div className="container">
+            <h3>iam a slider</h3>
+        </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default HomeSlider
