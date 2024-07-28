6266d3d777e55a018ef0df48452ab201d30ab3ce
suni <sunielsharma1921@gmail.com>
2024-07-28 15:58:47 +0545

    add login and register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import Layout from './layout/Layout';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
+import Login from './components/auth/login/Login';
+import Register from './components/auth/register/Register';
 
 const App = () => {
   return (
@@ -12,8 +15,11 @@ const App = () => {
       <Route path='/' element={<Layout/>}>
       <Route index element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
       </Route>
     </Routes>
+    <Toaster/>
   </Router>
 
   </>
--- frontend/src/components/topBanner/Banner.jsx
+++ frontend/src/components/topBanner/Banner.jsx
@@ -1,27 +1,89 @@
-import React from 'react'
-import '../topBanner/banner.css'
-import { NavLink } from 'react-router-dom'
+// import React from 'react'
+// import '../topBanner/banner.css'
+// import { NavLink } from 'react-router-dom'
+// import { FaShoppingCart } from "react-icons/fa";
+// import { FaUserLarge } from "react-icons/fa6";
+// import { IoCallSharp } from "react-icons/io5";
+// import { AiFillMessage } from "react-icons/ai";
+// const Banner = () => {
+//   return (
+//     <>
+//     <div className="bannerContainer">
+//         <div className="container">
+//             <div className="banner">
+//                 <img className='NepalTechlogo' src="/img/nti.png" alt="Nepal Tech Logo" />
+//                 <div className="bannerLink list">
+//                     {/* <NavLink to={'/cart'}><IoCallSharp className='icons'/>+977 98000000</NavLink> */}
+//                     <NavLink><FaUserLarge className='icons'/></NavLink>
+//                     <NavLink ><AiFillMessage className='icons'/></NavLink>
+//                     <NavLink to={'/cart'}><FaShoppingCart className='icons'/></NavLink>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+      
+//     </>
+//   )
+// }
+
+// export default Banner
+
+
+
+
+
+
+
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { FaShoppingCart } from "react-icons/fa";
-import { FaUserLarge } from "react-icons/fa6";
 import { IoCallSharp } from "react-icons/io5";
+import { AiFillMessage } from "react-icons/ai";
+import { Dropdown, Menu } from 'antd';
+import { FaUserAlt } from "react-icons/fa";
+import '../topBanner/banner.css';
+
+const userMenu = (
+  <Menu>
+    <Menu.Item key="1">
+      <NavLink to="/login">Client Login</NavLink>
+    </Menu.Item>
+    <Menu.Item key="2">
+      <NavLink to="/update-document">Update Document</NavLink>
+    </Menu.Item>
+  </Menu>
+);
+
+const messageMenu = (
+  <Menu>
+    <Menu.Item key="1">
+      <NavLink to="/new-order">New Order</NavLink>
+    </Menu.Item>
+    <Menu.Item key="2">
+      <NavLink to="/existing-order">Existing Order</NavLink>
+    </Menu.Item>
+  </Menu>
+);
+
 const Banner = () => {
   return (
-    <>
     <div className="bannerContainer">
-        <div className="container">
-            <div className="banner">
-                <img className='NepalTechlogo' src="/img/nti.png" alt="Nepal Tech Logo" />
-                <div className="bannerLink list">
-                    {/* <NavLink to={'/cart'}><IoCallSharp className='icons'/>+977 98000000</NavLink> */}
-                    <NavLink to={'/cart'}><FaShoppingCart className='icons'/></NavLink>
-                    <NavLink to={'/cart'}><FaUserLarge className='icons'/></NavLink>
-                </div>
-            </div>
+      <div className="container">
+        <div className="banner">
+          <img className='NepalTechlogo' src="/img/nti.png" alt="Nepal Tech Logo" />
+          <div className="bannerLink list">
+            <Dropdown overlay={userMenu} placement="bottomRight">
+              <NavLink><FaUserAlt className='icons' /></NavLink>
+            </Dropdown>
+            <Dropdown overlay={messageMenu} placement="bottomRight">
+              <NavLink><AiFillMessage className='icons' /></NavLink>
+            </Dropdown>
+            <NavLink to={'/cart'}><FaShoppingCart className='icons' /></NavLink>
+          </div>
         </div>
+      </div>
     </div>
-      
-    </>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
